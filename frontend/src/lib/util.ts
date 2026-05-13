6900d9f5d542bd934184bf67a8ee08517fe5c625
Junior Santos <jrs.ifs@gmail.com>
2026-05-12 21:22:18 -0300

    fix: fix date error, it was one day before

--- frontend/src/lib/util.ts
+++ frontend/src/lib/util.ts
@@ -1,5 +1,7 @@
 export function formatarData(data: string) {
-  return new Date(data).toLocaleDateString("pt-BR");
+  const oldData = new Date(data);
+  oldData.setDate(oldData.getDate() + 1);
+  return new Date(oldData).toLocaleDateString("pt-BR");
 }
 
 export function formatarMoeda(valor: number) {
