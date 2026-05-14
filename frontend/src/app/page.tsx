1f0fc5c2b3a41b061dc6484146752be2e9645a40
Junior Santos <jrs.ifs@gmail.com>
2026-05-14 18:59:51 -0300

    feat: implement Filtros component for managing request filters and add styles

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,43 +2,23 @@
 
 import styles from "./page.module.css";
 import ListaDeSolicitacoes from "../components/ListaDeSolicitacoes/page";
-import { useEffect, useState } from "react";
-import { obterSolicitacoes } from "../services/obterSolicitacoes";
-import { Solicitacao, StatusSolicitacao } from "../types/Solicitacao";
-import { obterCategorias } from "@/services/onterCategorias";
-import { Categoria } from "@/types/Categoria";
+import { useState } from "react";
 import { statusList } from "@/services/obterProximosStatus";
 import { FiltroSolicitacaoDTO } from "@/dtos/FiltroSolicitacoesDTO";
 import Modal from "@/components/Modal/page";
-import { Solicitante } from "@/types/Solicitante";
-import { obterSolicitantes } from "@/services/obterSolicitantes";
+import { useCategorias } from "@/hooks/useCategorias";
+import { useSolicitantes } from "@/hooks/useSolicitantes";
+import { useSolicitacoes } from "@/hooks/useSolicitacoes";
+import Filtros from "@/components/Filtros/page";
 
 export default function Home() {
-  const [solicitacoes, setSolicitacoes] = useState<Solicitacao[]>([]);
-  const [categorias, setCategorias] = useState<Categoria[]>([]);
   const [filtros, setFiltros] = useState<FiltroSolicitacaoDTO>({});
   const [modalAberto, setModalAberto] = useState(false);
-  const [solicitantes, setSolicitantes] = useState<Solicitante[]>([]);
 
-  useEffect(() => {
-    async function carregarSolicitacoes() {
-      const dados = await obterSolicitacoes(filtros);
-      setSolicitacoes(dados);
-    }
+  const categorias = useCategorias();
+  const solicitantes = useSolicitantes();
 
-    async function carregarCategorias() {
-      const dados = await obterCategorias();
-      setCategorias(dados);
-    }
-
-    async function carregarSolicitantes() {
-      setSolicitantes(await obterSolicitantes());
-    }
-
-    carregarSolicitantes();
-    carregarCategorias();
-    carregarSolicitacoes();
-  }, [filtros]);
+  const { solicitacoes, setSolicitacoes, loading } = useSolicitacoes(filtros);
 
   return (
     <div className={styles.page}>
@@ -49,69 +29,26 @@ export default function Home() {
             <p>Sistema de Gerenciamento de Soluções</p>
           </div>
           <div id={styles.inputContainer}>
-            <select
-              name="Selecione"
-              id={styles.select}
-              value={filtros?.categoriaId || ""}
-              onChange={(e) =>
-                setFiltros({ ...filtros, categoriaId: e.target.value })
-              }
-            >
-              <option>Categoria</option>
-              {categorias.map((item) => (
-                <option value={item.id} key={item.id}>
-                  {item.nome}
-                </option>
-              ))}
-            </select>
-
-            <input
-              type="date"
-              id={styles.select}
-              value={filtros.dataInicio || ""}
-              onChange={(e) =>
-                setFiltros({
-                  ...filtros,
-                  dataInicio: e.target.value,
-                })
-              }
-            />
-            <input
-              type="date"
-              id={styles.select}
-              value={filtros.dataFim || ""}
-              onChange={(e) =>
-                setFiltros({
-                  ...filtros,
-                  dataFim: e.target.value,
-                })
-              }
+            <Filtros
+              filtros={filtros}
+              setFiltros={setFiltros}
+              categorias={categorias}
             />
 
-            <select
-              name="Status"
-              id={styles.select}
-              value={filtros.status || ""}
-              onChange={(e) =>
-                setFiltros({
-                  ...filtros,
-                  status: e.target.value,
-                })
-              }
-            >
-              <option>Status</option>
-              {statusList.map((item) => (
-                <option key={item.id}>{item.status}</option>
-              ))}
-            </select>
-            <button
-              id={styles.button}
-              type="button"
-              onClick={() => setFiltros({})}
-            >
-              Limpar filtros
-            </button>
-            <div>
+            <div id={styles.containerButtons}>
+              <button
+                disabled={JSON.stringify(filtros) === JSON.stringify({})}
+                id={
+                  JSON.stringify(filtros) === JSON.stringify({})
+                    ? styles.buttonDisabled
+                    : styles.button
+                }
+                type="button"
+                onClick={() => setFiltros({})}
+              >
+                Limpar filtros
+              </button>
+
               <Modal
                 aberto={modalAberto}
                 fecharModal={() => setModalAberto(false)}
@@ -135,7 +72,7 @@ export default function Home() {
                 setSolicitacoes={setSolicitacoes}
               />
             ) : (
-              "Voce ainda não cadastrou nenhuma solicitação! Clique no botão Nova Solicitação para cadastrar."
+              "Ops! Parece que não há solicitações. Se isso for um erro, tente limpar os filtros ou criar uma nova solicitação."
             )}
           </div>
         </div>
---
+++ frontend/src/components/Filtros/page.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { Categoria } from "@/types/Categoria";
+import { FiltroSolicitacaoDTO } from "@/dtos/FiltroSolicitacoesDTO";
+import { statusList } from "@/services/obterProximosStatus";
+import styles from "./filtros.module.css";
+
+type Props = {
+  filtros: FiltroSolicitacaoDTO;
+  setFiltros: React.Dispatch<React.SetStateAction<FiltroSolicitacaoDTO>>;
+  categorias: Categoria[];
+};
+
+export default function Filtros({ filtros, setFiltros, categorias }: Props) {
+  function atualizarFiltro(campo: string, valor: string) {
+    setFiltros((prev) => ({
+      ...prev,
+      [campo]: valor,
+    }));
+  }
+
+  return (
+    <>
+      <div id={styles.dateInputContainer}>
+        <label id={styles.label}>Categoria</label>
+        <select
+          id={styles.select}
+          value={filtros.categoriaId || ""}
+          onChange={(e) => atualizarFiltro("categoriaId", e.target.value)}
+        >
+          <option value="">Categoria</option>
+
+          {categorias.map((categoria) => (
+            <option key={categoria.id} value={categoria.id}>
+              {categoria.nome}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div id={styles.dateInputContainer}>
+        <label id={styles.label}>Data Inicio</label>
+        <input
+          id={styles.input}
+          type="date"
+          value={filtros.dataInicio || ""}
+          onChange={(e) => atualizarFiltro("dataInicio", e.target.value)}
+        />
+      </div>
+
+      <div id={styles.dateInputContainer}>
+        <label id={styles.label}>Data Fim</label>
+        <input
+          id={styles.input}
+          type="date"
+          value={filtros.dataFim || ""}
+          onChange={(e) => atualizarFiltro("dataFim", e.target.value)}
+        />
+      </div>
+      <div id={styles.dateInputContainer}>
+        <label id={styles.label}>Status</label>
+        <select
+          id={styles.select}
+          value={filtros.status || ""}
+          onChange={(e) => atualizarFiltro("status", e.target.value)}
+        >
+          <option value="">Status</option>
+
+          {statusList.map((item) => (
+            <option key={item.id} value={item.status}>
+              {item.status}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
+  );
+}
